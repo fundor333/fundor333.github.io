fe91cef96d4f5e5e74543255cec7eb3278fb489f
Fundor333 <fundor333@fundor333.com>
2025-04-14 15:28:53 +0200

    Update Hugo version and refactor post creation functions to use name cleaning

--- make-post.py
+++ make-post.py
@@ -1,31 +1,38 @@
 import datetime
 import os
+import re
+
+
+def name_cleaning(name: str) -> str:
+
+    title = re.sub("[^A-Za-z0-9 ]+", " ", name)
+    title = title.replace("  ", " ")
+    title = title.replace(" ", "-")
+    title = title.lower()
+    return title
 
 
 def post_photo():
     # get the current year as variable
     year = str(datetime.datetime.now().year)
     name = input("Give me the title\n")
-    os.system(
-        f"hugo new photos/{year}/{name.replace(' ', '-').replace(',', '').lower()}/index.md"
-    )
+    title = name_cleaning(name)
+    os.system(f"hugo new photos/{year}/{title}/index.md")
 
 
 def post_redirect():
     # get the current year as variable
     name = input("Give me the title\n")
-    os.system(
-        f"hugo new redirect/{name.replace(' ', '-').replace(',', '').lower()}/index.md"
-    )
+    title = name_cleaning(name)
+    os.system(f"hugo new redirect/{title}/index.md")
 
 
 def post_fc():
     # get the current year as variable
     year = str(datetime.datetime.now().year)
     name = input("Give me the title\n")
-    os.system(
-        f"hugo new post/{year}/{name.replace(' ', '-').replace(',', '').lower()}/index.md"
-    )
+    title = name_cleaning(name)
+    os.system(f"hugo new post/{year}/{title}/index.md")
 
 
 def micro_fc():
@@ -33,7 +40,9 @@ def micro_fc():
     name = input("Give me the title\n")
     year = str(datetime.datetime.now().year)
     month = str(datetime.datetime.now().month)
-    generated = f"{year.ljust(4, "0")}/{month.ljust(2, "0")}/{name.replace(' ', '-').replace(',', '').lower()}"
+    title = name_cleaning(name)
+
+    generated = f"{year.ljust(4, "0")}/{month.ljust(2, "0")}/{title}"
 
     os.system(f"hugo new micro/{generated}/index.md")
     print(f"Generated {generated}/index.md")
