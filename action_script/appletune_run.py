61c5af4c049bb767b8f9085eb798e4cadd280ba1
Fundor333 <fundor333@fundor333.com>
2026-01-28 15:49:43 +0100

    Add cron job and script for generating expenses

---
+++ action_script/appletune_run.py
@@ -0,0 +1,13 @@
+import requests
+
+url_api = "https://fundor333.com/pantalone/genera-spese"
+
+# Call the API to generate expenses
+response = requests.get(url_api)
+if response.status_code == 200:
+    print("Expenses generated successfully.")
+else:
+    print(f"Failed to generate expenses. Status code: {response.status_code}")
+    # raise an error for the github action to fail
+
+    raise Exception(f"API call failed with status code: {response.status_code}\n")
