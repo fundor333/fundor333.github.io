96ffffb5d346380accb9006875cc184ebc484898
Fundor333 <fundor333@fundor333.com>
2026-06-15 00:53:07 +0200

    Add new icon files for various features
    - Added cssdif.gif to the 88x31 icons directory.
    - Added fascist.png to the 88x31 icons directory.
    - Added feed.gif to the 88x31 icons directory.
    - Added fundor333.gif to the 88x31 icons directory.
    - Added nintendo_switch.png to the 88x31 icons directory.
    - Added sh.tangled.git.temp.gif to the 88x31 icons directory.

--- micro_meetup.py
+++ micro_meetup.py
@@ -1,10 +1,11 @@
-import requests
 import datetime
-import os
-import re
-import typer
-from typing import Annotated
 import json
+import re
+from pathlib import Path
+from typing import Annotated
+
+import requests
+import typer
 
 url = "https://api.meetup.com/gql-ext"
 
@@ -49,19 +50,17 @@ def fetch_event(event_id):
     r = requests.post(url, json={"query": query, "variables": variables})
     data = r.json()
     data["id"] = event_id
-    data["now"] = (
-        datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=36500)
-    ).strftime("%Y-%m-%dT%H:%M:%S")
+    data["now"] = (datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=36500)).strftime("%Y-%m-%dT%H:%M:%S")
     return data
 
 
 def convert_dict_to_json(data):
     # convert the json to a pretty json and save it in data/event/event_id.json
-    os.makedirs("data/event", exist_ok=True)
+    Path("data/event").mkdir(parents=True, exist_ok=True)
     event = data["data"]["event"]
     event_id = re.search(r"events/(\d+)", event["eventUrl"]).group(1)
     filename = f"data/event/{event_id}.json"
-    with open(filename, "w") as f:
+    with Path.open(filename, "w") as f:
         json.dump(data, f, indent=4, sort_keys=True, default=serialize_datetime)
 
 
@@ -70,9 +69,7 @@ def convert_dict_to_post(data):
     now = data["now"]
     event = data["data"]["event"]
     title = event["title"]
-    date_time_file = datetime.datetime.fromisoformat(
-        event["dateTime"].replace("Z", "")
-    ).strftime("%Y-%m-%d")
+    date_time_file = datetime.datetime.fromisoformat(event["dateTime"].replace("Z", "")).strftime("%Y-%m-%d")
     description = event["description"]
     date_time = datetime.datetime.fromisoformat(event["dateTime"].replace("Z", ""))
     end_time = datetime.datetime.fromisoformat(event["endTime"].replace("Z", ""))
@@ -108,26 +105,26 @@ how_to_find_us: {how_to_find_us}
 ---\n"""
     post += f"{description}\n"
     # create the content/event directory if it doesn't exist
-    os.makedirs("content/event", exist_ok=True)
+    Path("content/event").mkdir(parents=True, exist_ok=True)
     # write to a markdown file where is in the format content/event/YYYY-MM-DD-event-title.md
 
     filename = f"content/event/{date_time_file}-{re.sub(r'[^a-zA-Z0-9]+', '-', title).strip('-').lower()}.md"
-    with open(filename, "w") as f:
+    with Path.open(filename, "w") as f:
         f.write(post)
 
 
 def add_to_memory(text: str):
-    # Save in /data/memory_meetup.txt, if not already present, the url pass as argument
-    os.makedirs("data", exist_ok=True)
-    memory_file = "data/memory_meetup.txt"
-    if not os.path.exists(memory_file):
-        with open(memory_file, "w") as f:
+    # Save in action_script//data/memory_meetup.txt, if not already present, the url pass as argument
+    Path("action_script/data").mkdir(parents=True, exist_ok=True)
+    memory_file = "action_script/data/memory_meetup.txt"
+    if not Path(memory_file).exists():
+        with Path.open(memory_file, "w") as f:
             f.write("")
 
-    with open(memory_file) as f:
+    with Path.open(memory_file) as f:
         memory = f.read().splitlines()
     if text not in memory:
-        with open(memory_file, "a") as f:
+        with Path.open(memory_file, "a") as f:
             f.write(text + "\n")
         print(f"Added {text} to memory.")
     else:
@@ -136,18 +133,15 @@ def add_to_memory(text: str):
 
 def fetch_event_from_json(event_id):
     filename = f"data/event/{event_id}.json"
-    if not os.path.exists(filename):
-        print(
-            f"No JSON file found for event ID {event_id}. Now Fetching from Meetup..."
-        )
+    if not Path(filename).exists():
+        print(f"No JSON file found for event ID {event_id}. Now Fetching from Meetup...")
         add_to_memory(event_id)
         data = fetch_event(event_id)
         convert_dict_to_json(data)
         data = fetch_event_from_json(event_id)
         convert_dict_to_post(data)
-    with open(filename) as f:
-        data = json.load(f)
-    return data
+    with Path.open(filename) as f:
+        return json.load(f)
 
 
 def main(
@@ -155,8 +149,8 @@ def main(
     memory: Annotated[bool, typer.Option("--memory", "-m")] = False,
 ):
     if memory:
-        if os.path.exists("data/memory_meetup.txt"):
-            with open("data/memory_meetup.txt") as f:
+        if Path("action_script/data/memory_meetup.txt").exists():
+            with Path("action_script/data/memory_meetup.txt").open() as f:
                 memory = f.read().splitlines()
             for item in memory:
                 print(f"Fetching event {item} from memory...")
